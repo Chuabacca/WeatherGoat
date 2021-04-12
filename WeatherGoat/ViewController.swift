d67b79fa54ec4a50dc5fb9a80978592e482ac058
Jonathan Chua <chuabacca@gmail.com>
2021-04-12 15:42:36 -0700

    Initial Commit

---
+++ WeatherGoat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGoat
+//
+//  Created by Jon on 4/12/21.
+//  Copyright © 2021 Jonathan Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
